e5c17fe8c0eecbefb0d92891cfe017f79a1db15b
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-10-20 21:11:01 +0500

    Task1

---
+++ Tyuiu.FrankoVA.Sprint3.Task1.V18/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.FrankoVA.Sprint3.Task1.V18.Lib;
+namespace Tyuiu.FrankoVA.Sprint3.Task1.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3| Выполнила: Франко В.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Франко В.А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда   *");
+            Console.WriteLine("* по формуле                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 15;
+            Console.WriteLine("Начало шага " + startValue);
+            Console.WriteLine("Конец шага " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
